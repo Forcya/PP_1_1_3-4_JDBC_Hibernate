0ebd4801aae6af289219260e135f107661902e5e
Forcya <74520011+Forcya@users.noreply.github.com>
2022-03-31 23:17:04 +0300

    Реализация методов с помощью Hibernate

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,9 @@
 package jm.task.core.jdbc;
 
 import jm.task.core.jdbc.dao.UserDao;
+import jm.task.core.jdbc.dao.UserDaoHibernateImpl;
 import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
+import jm.task.core.jdbc.model.User;
 import jm.task.core.jdbc.util.Util;
 
 
@@ -9,7 +11,7 @@ public class Main {
 
     public static void main(String[] args) {
         Util.getConnection();
-        UserDao userDao = new UserDaoJDBCImpl();
+        UserDao userDao = new UserDaoHibernateImpl();
 
         userDao.createUsersTable();
 
@@ -18,9 +20,11 @@ public class Main {
         userDao.saveUser("Name3", "LastName3", (byte) 31);
         userDao.saveUser("Name4", "LastName4", (byte) 38);
 
-        userDao.removeUserById(2);
+        userDao.removeUserById(4);
         userDao.getAllUsers();
         userDao.cleanUsersTable();
         userDao.dropUsersTable();
+
+
     }
 }
--- src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -1,42 +1,105 @@
 package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.Util;
 
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.List;
 
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
 public class UserDaoHibernateImpl implements UserDao {
+
+    private static String CREATE = "CREATE TABLE IF NOT EXISTS Users(id INT primary key AUTO_INCREMENT, name VARCHAR(15), lastName VARCHAR(20), age INT);";
+    private static String DELETE_TABLE = "DROP TABLE IF EXISTS Users;";
+    Statement statement;
+    Util util = new Util();
+
     public UserDaoHibernateImpl() {
 
     }
 
-
     @Override
     public void createUsersTable() {
-
+        try {
+            statement = util.getConnection().createStatement();
+            statement.execute(CREATE);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public void dropUsersTable() {
-
+        try {
+            statement = util.getConnection().createStatement();
+            statement.execute(DELETE_TABLE);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public void saveUser(String name, String lastName, byte age) {
 
+        Transaction transaction = null;
+        try (Session session = Util.getSessionFactory().openSession()) {
+
+            User user = new User(name, lastName, age);
+            transaction = session.beginTransaction();
+            session.save(user);
+            transaction.commit();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public void removeUserById(long id) {
+        Transaction transaction = null;
+        try (Session session = Util.getSessionFactory().openSession()) {
+            transaction = session.beginTransaction();
+            User user = session.get(User.class, id);
+            session.delete(user);
+            transaction.commit();
 
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public List<User> getAllUsers() {
-        return null;
+        Transaction transaction = null;
+        List<User> listUser = null;
+        try (Session session = Util.getSessionFactory().openSession()) {
+            transaction = session.beginTransaction();
+
+//            listUser = session.createQuery("from User").getResultList();
+//            transaction.commit();
+
+            listUser = session.createQuery("from User").getResultList();
+            for (User p : listUser) {
+                System.out.println(p);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return listUser;
     }
 
     @Override
     public void cleanUsersTable() {
-
+        Transaction transaction = null;
+        try (Session session = Util.getSessionFactory().openSession()) {
+            transaction = session.beginTransaction();
+            session.createQuery("delete User").executeUpdate();
+            transaction.commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
