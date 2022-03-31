f2733c017f503a24b85818b049445d283b3ecf65
Forcya <74520011+Forcya@users.noreply.github.com>
2022-04-01 00:23:53 +0300

    Убрал лишние, закоменченные строки кода

--- src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -16,6 +16,7 @@ public class UserDaoHibernateImpl implements UserDao {
     private static String DELETE_TABLE = "DROP TABLE IF EXISTS Users;";
     Statement statement;
     Util util = new Util();
+    Transaction transaction = null;
 
     public UserDaoHibernateImpl() {
 
@@ -44,9 +45,7 @@ public class UserDaoHibernateImpl implements UserDao {
     @Override
     public void saveUser(String name, String lastName, byte age) {
 
-        Transaction transaction = null;
         try (Session session = Util.getSessionFactory().openSession()) {
-
             User user = new User(name, lastName, age);
             transaction = session.beginTransaction();
             session.save(user);
@@ -59,7 +58,6 @@ public class UserDaoHibernateImpl implements UserDao {
 
     @Override
     public void removeUserById(long id) {
-        Transaction transaction = null;
         try (Session session = Util.getSessionFactory().openSession()) {
             transaction = session.beginTransaction();
             User user = session.get(User.class, id);
@@ -73,14 +71,9 @@ public class UserDaoHibernateImpl implements UserDao {
 
     @Override
     public List<User> getAllUsers() {
-        Transaction transaction = null;
         List<User> listUser = null;
         try (Session session = Util.getSessionFactory().openSession()) {
             transaction = session.beginTransaction();
-
-//            listUser = session.createQuery("from User").getResultList();
-//            transaction.commit();
-
             listUser = session.createQuery("from User").getResultList();
             for (User p : listUser) {
                 System.out.println(p);
@@ -93,7 +86,6 @@ public class UserDaoHibernateImpl implements UserDao {
 
     @Override
     public void cleanUsersTable() {
-        Transaction transaction = null;
         try (Session session = Util.getSessionFactory().openSession()) {
             transaction = session.beginTransaction();
             session.createQuery("delete User").executeUpdate();
